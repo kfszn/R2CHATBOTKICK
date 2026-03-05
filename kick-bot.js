8bbd3322a8d590654cc27ec636295dc2e7fcc6a7
kfszn <80652955+kfszn@users.noreply.github.com>
2026-03-04 23:06:28 -0500

    Add files via upload

--- kick-bot.js
+++ kick-bot.js
@@ -116,7 +116,7 @@ function handleMessage(data) {
 function connect() {
   log(`[ws] Connecting to Kick chat for channel: ${KICK_CHANNEL}`);
 
-  ws = new WebSocket('wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.6.0&flash=false');
+  ws = new WebSocket('wss://ws-us2.pusher.com/app/32cbd69e4b950bf97679?protocol=7&client=js&version=7.6.0&flash=false');
 
   ws.on('open', () => {
     log('[ws] Connected to Pusher');
